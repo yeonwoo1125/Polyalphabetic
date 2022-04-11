681eeb0ea675fb0347710663ab21bb4135b24ace
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-04-11 21:13:26 +0900

    Initial commit

---
+++ src/test/java/com/school/protect/polyalphabetic/PolyalphabeticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.protect.polyalphabetic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyalphabeticApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
