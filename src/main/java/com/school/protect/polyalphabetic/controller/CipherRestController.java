93d8d2c7a7fc934211eb6b12b03bbcad5da145dc
가연우 <81597104+yeonwoo1125@users.noreply.github.com>
2022-04-14 23:16:50 +0900

    [ADD] 폼에 데이터 입력하면 키, 평문, 매핑, 암호문 출력

---
+++ src/main/java/com/school/protect/polyalphabetic/controller/CipherRestController.java
@@ -0,0 +1,23 @@
+package com.school.protect.polyalphabetic.controller;
+
+import com.school.protect.polyalphabetic.domain.Cipher;
+import com.school.protect.polyalphabetic.dto.request.CipherRequestDto;
+import com.school.protect.polyalphabetic.service.CipherService;
+import lombok.AllArgsConstructor;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+@RestController
+@AllArgsConstructor
+public class CipherRestController {
+    private CipherService cipherService;
+
+}
--- src/main/java/com/school/protect/polyalphabetic/controller/CipherWebController.java
+++ src/main/java/com/school/protect/polyalphabetic/controller/CipherWebController.java
@@ -1,6 +1,5 @@
 package com.school.protect.polyalphabetic.controller;
 
-
 import com.school.protect.polyalphabetic.domain.Cipher;
 import com.school.protect.polyalphabetic.dto.request.CipherRequestDto;
 import com.school.protect.polyalphabetic.service.CipherService;
@@ -8,6 +7,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @AllArgsConstructor
@@ -15,17 +15,26 @@ public class CipherWebController {
 
     private CipherService cipherService;
 
-    @PostMapping()
+    @GetMapping()
     public String mainPage(@ModelAttribute("form") CipherRequestDto dto){
         return "main";
     }
 
-    @PostMapping( "/cipher")
-    public String cipherPage(Model m, CipherRequestDto dto){
+    @PostMapping("/cipher")
+    public String sendCipherId(CipherRequestDto dto, RedirectAttributes redirectAttributes){
         Long id = cipherService.save(dto);
+        redirectAttributes.addAttribute("id",id);
+        return "redirect:/{id}";
+    }
+
+    @GetMapping("/{id}")
+    public String showCipher(@PathVariable Long id, Model m){
         Cipher cipher = cipherService.getCipher(id);
         m.addAttribute("key", cipher.getKey());
         m.addAttribute("value",cipher.getValue());
-        return "cipher";
+        m.addAttribute("encryption", cipher.getEncryption());
+        m.addAttribute("cryptogram", cipher.getCryptogram());
+
+        return "showCipher";
     }
 }
